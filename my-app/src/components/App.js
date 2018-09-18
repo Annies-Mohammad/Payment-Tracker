ebaf8c3820a3ecf24ae4dffb8084e2da729b3ef2
Huy Van Vu <huy.vanvu@ofx.com>
2018-09-18 16:32:05 +1000

    initial tracker

---
+++ my-app/src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Tracker from './tracker';
+import logo from '../logo.svg';
+import '../css/App.css';
+import '../css/site.min.css';
+import Body1 from '../mock/body1';
+import Body2 from '../mock/body2';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Tracker/>
+          <p className="App-intro">
+            
+          </p>
+        </div>
+        <Body1/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/fonts/icon-font.md
@@ -0,0 +1,7 @@
+# Icon Font
+
+The *ofx-icons* font is generated with [IcoMoon](https://icomoon.io/app)
+
+Use the `selection.json` file to import the Project back into IcoMoon. Make your changes. Export the Font. Update the classes in `_fonts.scss` with the css file provided in the Font Export.
+
+Also replace the files inside the fonts folder with the files in the new exported fonts folder. Converting the woff file to woff2 using an online converter is also required.
