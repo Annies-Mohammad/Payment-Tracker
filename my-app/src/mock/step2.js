61ea4ffee032a757416f662bd6fb3d5397327c3e
OlaSalem <ola.salem@ofx.com>
2018-09-19 14:21:05 +1000

    adding a newline

--- my-app/src/mock/step2.js
+++ my-app/src/mock/step2.js
@@ -5,6 +5,7 @@ class Step2 extends Component {
     return (
       <div>
          <p><b>We are waiting for your funds.Your bank may take up to 24 hours to send your funds to us. Once we have received your funds we will let you know.
+         <br></br>
           Want faster payments? <a>Click Here</a> to set up Direct Debit for your next transfer.</b></p>
       </div>
     );
