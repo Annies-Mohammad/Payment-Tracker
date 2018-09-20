bad1822d519842080baf7379ccd3ecd0f6bc23d4
OZFOREX\mohamman <annies.mohammad@ofx.com>
2018-09-20 13:45:12 +1000

    wthout key

--- PaymentTracker/ConsoleApp1/Program.cs
+++ PaymentTracker/ConsoleApp1/Program.cs
@@ -9,28 +9,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+           
+            ExecuteFinal().Wait();
             Execute().Wait();
-            Console.Read();
+
+        }
+       
+        static async Task ExecuteFinal()
+        {
+            var apiKey1 = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client1 =
+                new SendGridClient("");
+            var from1 = new EmailAddress("customer.service@ofx.com", "OFX | Customer Service ");
+            var subject1 = "OFX | Transfer sent to receipient";
+            var to1 = new EmailAddress("Sammy.Won@ofx.com", "Track-r");
+            //annies.mohammad @ofx.com; ola.Salem @ofx.com; Huy.VanVu @ofx.com;Sammy.Won @ofx.com 
+            var plainTextContent1 = "and easy to do anywhere, even with C#";
+            var htmlContent1 = System.IO.File.ReadAllText(@"C:\dev\Payment-Tracker\PaymentTracker\ConsoleApp1\2.html");
+            var msg1 = MailHelper.CreateSingleEmail(from1, to1, subject1, plainTextContent1, htmlContent1);
+            var response1 = await client1.SendEmailAsync(msg1);
+
         }
         static async Task Execute()
         {
+
             var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
-            var client = new SendGridClient("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");
-            var from = new EmailAddress("Huy.VanVu@ofx.com", "Example User");
-            var subject = "Payment tracker stage 1";
-            var to = new EmailAddress("Jessica.Crocker@ofx.com", "Example User");
+            var client = new SendGridClient("");
+            var from = new EmailAddress("customer.service@ofx.com", "OFX | Customer Service ");
+            var subject = "OFX | Payment Receipt (MT103)";
+            var to = new EmailAddress("Sammy.Won@ofx.com", "Track-r");
             //annies.mohammad @ofx.com; ola.Salem @ofx.com; Huy.VanVu @ofx.com;Sammy.Won @ofx.com 
             var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = letterContent();
+            var htmlContent = System.IO.File.ReadAllText(@"C:\dev\Payment-Tracker\PaymentTracker\ConsoleApp1\1.html");
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg);
-        }
-
-        internal static string letterContent()
-        {
-            string text = System.IO.File.ReadAllText(@"C:\dev\Payment-Tracker\PaymentTracker\ConsoleApp1\TextFile1.txt");
-            return text;
         }
     }
 }
